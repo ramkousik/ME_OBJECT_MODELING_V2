9c4627e4c576d1323af2845971907331cfe4d063
CrioUser <crio-user@criodo.com>
2022-11-24 17:43:03 +0530

    ME_OBJECT_MODELING_V2_MODULE_TAKEHOME

--- src/main/java/com/crio/jukebox/App.java
+++ src/main/java/com/crio/jukebox/App.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+import com.crio.jukebox.appConfig.ApplicationConfig;
+import com.crio.jukebox.commands.CommandInvoker;
+import com.crio.jukebox.exceptions.NoSuchCommandException;
 
 
 public class App {
@@ -24,6 +27,25 @@ public class App {
 
     public static void run(List<String> commandLineArgs) {
     // Complete the final logic to run the complete program.
+    ApplicationConfig applicationConfig = new ApplicationConfig();
+    CommandInvoker commandInvoker = applicationConfig.getCommandInvoker();
+    BufferedReader reader;
+    String inputFile = commandLineArgs.get(0).split("=")[1];
+    System.out.println("inputfile"+inputFile);
+    commandLineArgs.remove(0);
+    try {
+        reader = new BufferedReader(new FileReader(inputFile));
+        String line = reader.readLine();
+        while (line != null) {
+            List<String> tokens = Arrays.asList(line.split(" "));
+            commandInvoker.executeCommand(tokens.get(0),tokens);
+            // read next line
+            line = reader.readLine();
+        }
+        reader.close();
+    } catch (IOException | NoSuchCommandException e) {
+        e.printStackTrace();
+    }
 
     }
 }
